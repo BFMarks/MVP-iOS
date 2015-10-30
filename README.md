d5fcb300ff636fdd659531ccc96c4ad49e01349a
owaysisapp <bryanfmarks@gmail.com>
2015-10-30 13:57:35 -0700

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+For Wayfarer Mobile Tech Only.
---
+++ WayfarerMobile/cloud/main.js
@@ -0,0 +1,13 @@
+
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:  GO FUCK YOURSELF PARSE
+
+//
+//Parse.Cloud.define("hello", function(request, response) {
+//  response.success("Hello world!");
+//});
+
+require('cloud/installation.js');
+require('cloud/activity.js');
+require('cloud/photo.js');
+
